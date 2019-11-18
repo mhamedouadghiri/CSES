2cdb2bf8d97b53b3b177296b1769f006afbd2fe7
mhamedouadghiri <mhamed.sefianeelouadghiri@gmail.com>
2019-11-18 11:50:11 +0100

    Initial Commit

---
+++ src/DynamicProgramming/RemovingDigits.java
@@ -0,0 +1,58 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class RemovingDigits {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out);
+        int n = nextInt();
+        int cnt = 0;
+        while (n != 0) {
+            n -= Arrays.stream(String.valueOf(n).split("")).mapToInt(Integer::parseInt).max().getAsInt();
+            ++cnt;
+        }
+        pw.println(cnt);
+        pw.close();
+    }
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+
+    private static String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    private static String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    private static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    private static long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    private static double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
