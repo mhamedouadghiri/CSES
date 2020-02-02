f64a54f1673f7ab8f7c82d0e1c9696be09d281ed
mhamedouadghiri <mhamed.sefianeelouadghiri@gmail.com>
2020-02-02 19:19:55 +0100

    Initial Commit

---
+++ src/SortingAndSearching/FerrisWheel.java
@@ -0,0 +1,65 @@
+package SortingAndSearching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class FerrisWheel {
+    private static final long MOD = 1000000007;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out);
+        int n = nextInt();
+        int x = nextInt();
+        Integer[] W = new Integer[n];
+        for (int i = 0; i < n; i++)
+            W[i] = nextInt();
+        Arrays.sort(W);
+        int cnt = 0, i = 0, j = n - 1;
+        while (i <= j) {
+            if (W[i] + W[j] <= x) i++;
+            j--;
+            cnt++;
+        }
+        pw.println(cnt);
+        pw.close();
+    }
+
+    private static String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    private static String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    private static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    private static long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    private static double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
